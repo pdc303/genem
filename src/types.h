f8587f5881607d973b85f7ac44baead84b295808
Paul Critchley <pdc09@pdc.me.uk>
2010-03-15 18:21:07 +0000

    Added clock cycle type

--- src/types.h
+++ src/types.h
@@ -2,6 +2,7 @@
 #define __TYPES_H__
 
 #include <stdint.h>
+#include <inttypes.h>
 
 typedef int32_t glong;
 typedef uint32_t gulong;
@@ -12,5 +13,7 @@ typedef int8_t gint;
 typedef uint8_t guint;
 
 typedef unsigned char byte;
+
+typedef uint64_t gclock_t;
 
 #endif /* __TYPES_H__ */
